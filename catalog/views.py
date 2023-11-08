fe5e74fa913870585907f5c89ead3e88729855f3
Valeriya Dudchenko <vvd.2209@bk.ru>
2023-11-08 16:59:54 +0300

    19.2

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name} ({email}): {message}')
+    return render(request, 'catalog/index.html')
