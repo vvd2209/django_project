b1620605a58d3ded96b211f0d99f329e43337f96
Valeriya Dudchenko <vvd.2209@bk.ru>
2023-12-01 18:47:40 +0300

    21.1 FBV и CBV

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView
+from blog.views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView, BlogDeleteView, toggle_activity
 
 app_name = BlogConfig.name
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('view/<int:pk>/', BlogDetailView.as_view(), name='view'), # посмотреть блог
     path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit'), # редактировать блог
     path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete'), # удалить блог
+    path('activity/<int:pk>/', toggle_activity, name='toggle_activity'), # активация/деактивация блога
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from pytils.translit import slugify
@@ -31,19 +31,32 @@ class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('blog:list')
 
+def toggle_activity(request, pk):
+    """ Отображает смену статуса статьи блога 'активно'/'неактивно'"""
+    blog_item = get_object_or_404(Blog, pk=pk)
+    if blog_item.is_publish:
+        blog_item.is_publish = False
+    else:
+        blog_item.is_publish = True
+
+    blog_item.save()
+
+    return redirect(reverse('blog:list'))
 
 class BlogListView(ListView):
     model = Blog
 
-    def get_queryset(self, *args, **kwargs):
-        queryset = super().get_queryset(*args, **kwargs)
-        queryset = queryset.filter(is_publish=True)
-        return queryset
+    # def get_queryset(self, *args, **kwargs):
+    #     """ Отображает только опубликованные(активированные) статьи блога """
+    #     queryset = super().get_queryset(*args, **kwargs)
+    #     queryset = queryset.filter(is_publish=True)
+    #     return queryset
 
 class BlogDetailView(DetailView):
     model = Blog
 
     def get_object(self, queryset=None):
+        """ При открытии статьи увеличивает счетчик просмотров статьи блога """
         self.object = super().get_object(queryset)
         self.object.count_view += 1
         self.object.save()
