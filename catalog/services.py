360a8d453cfcf3aaa79da83646051754542e0bd9
Valeriya Dudchenko <vvd.2209@bk.ru>
2024-01-22 18:40:41 +0300

    23.2 Кеширование и работа с переменными окружения

--- catalog/services.py
+++ catalog/services.py
@@ -8,21 +8,22 @@ def get_cached_version_for_product(product_pk):
     if settings.CACHE_ENABLED:
         key = f'version_list_{product_pk}'
         version_list = cache.get(key)
-        if version_list is None:
+        if not version_list:
             version_list = Version.objects.filter(product__pk=product_pk)
             cache.set(key, version_list)
     else:
         version_list = Version.objects.filter(product__pk=product_pk)
     return version_list
 
+
 def get_categories_cache():
     if settings.CACHE_ENABLED:
         key = 'category_list'
         category_list = cache.get(key)
-        if category_list in None:
+        if not category_list:
             category_list = Category.objects.all()
             cache.set(key, category_list)
     else:
-            category_list = Category.objects.all()
+        category_list = Category.objects.all()
 
     return category_list
